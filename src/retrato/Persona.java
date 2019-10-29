5659763c2215e2f784279424997d2155a0d80a64
eblan <eblan@LAPTOP-EAP6ISDE>
2019-10-29 16:53:52 -0500

    Retrato Hablado-Emerson Blanco-Nicolas Fonseca-Luis Correa

---
+++ src/retrato/Persona.java
@@ -0,0 +1,40 @@
+package retrato;
+
+/**
+ *
+ * @author eblan
+ */
+public class Persona {
+   private String nombre;
+   private int id;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, int id) {
+        this.nombre = nombre;
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre: " + nombre + "\n Id: " + id; 
+    }
+   
+}
